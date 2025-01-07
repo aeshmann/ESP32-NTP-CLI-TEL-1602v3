d92c7105f12d272a0e46eb7ec2a72b5edf24fcca
Const <dxconst@gmail.com>
2025-01-07 20:37:30 +0300

    upd

--- src/cwifi.h
+++ src/cwifi.h
@@ -3,5 +3,5 @@ WiFi credentials is allocatel in a separate file to add it to .gitignore
 */
 
 
-const char *mssid = "Xiaomi_065C";
-const char *mpass = "43v3ry0nG";
+const char *mssid = "SSID";
+const char *mpass = "PASS";
